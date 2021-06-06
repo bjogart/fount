694d6645a9b004f2390afd51429eaa9ce9d24d05
BJogart <jasper.opti@gmail.com>
2021-06-07 01:10:02 +0200

    orThrow() wasn't static

--- src/fount/core/OutcomeTools.hx
+++ src/fount/core/OutcomeTools.hx
@@ -3,7 +3,7 @@ package fount.core;
 using tink.CoreApi;
 
 class OutcomeTools {
-    public function orThrow<D, F, E>(outcome: Outcome<D, F>, onFailure: F -> E): D {
+    public static function orThrow<D, F, E>(outcome: Outcome<D, F>, onFailure: F -> E): D {
         return switch outcome {
             case Success(v): v;
             case Failure(v): throw onFailure(v);
