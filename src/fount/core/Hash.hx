0c0716f6ef30f80f3c41cbb7ccb630c46e7dab63
BJogart <jasper.opti@gmail.com>
2021-06-23 20:18:25 +0200

    inline state fns

--- src/fount/core/Hash.hx
+++ src/fount/core/Hash.hx
@@ -30,16 +30,16 @@ interface IHasher {
     function hash(): Int64;
 }
 
-private class Sip24State {
+private class SipState {
     static final INIT0 = Int64.make(0x736f6d65, 0x70736575);
     static final INIT1 = Int64.make(0x646f7261, 0x6e646f6d);
     static final INIT2 = Int64.make(0x6c796765, 0x6e657261);
     static final INIT3 = Int64.make(0x74656462, 0x79746573);
 
-    var v0: Int64;
-    var v1: Int64;
-    var v2: Int64;
-    var v3: Int64;
+    public var v0: Int64;
+    public var v1: Int64;
+    public var v2: Int64;
+    public var v3: Int64;
 
     public function new(k0: Int64, k1: Int64) {
         this.v0 = k0 ^ INIT0;
@@ -47,45 +47,11 @@ private class Sip24State {
         this.v2 = k0 ^ INIT2;
         this.v3 = k1 ^ INIT3;
     }
-
-    inline function transform(): Void {
-        v0 += v1;
-        v1 = v1.rol(13);
-        v1 ^= v0;
-        v0 = v0.rol(32);
-        v2 += v3;
-        v3 = v3.rol(16);
-        v3 ^= v2;
-        v0 += v3;
-        v3 = v3.rol(21);
-        v3 ^= v0;
-        v2 += v1;
-        v1 = v1.rol(17);
-        v1 ^= v2;
-        v2 = v2.rol(32);
-    }
-
-    public function compress(word: Int64): Void {
-        v3 ^= word;
-        transform();
-        transform();
-        v0 ^= word;
-    }
-
-    public function finalize(): Int64 {
-        v2 ^= 0xff;
-        transform();
-        transform();
-        transform();
-        transform();
-        return v0 ^ v1 ^ v2 ^ v3;
-    }
 }
 
 class Sip24 implements IHasher {
     final buf: BytesBuffer;
-    final k0: Int64;
-    final k1: Int64;
+    final state: SipState;
 
     public static inline function empty(): Sip24 {
         return new Sip24(0, 0);
@@ -97,8 +63,7 @@ class Sip24 implements IHasher {
 
     function new(k0: Int64, k1: Int64) {
         this.buf = new BytesBuffer();
-        this.k0 = k0;
-        this.k1 = k1;
+        this.state = new SipState(k0, k1);
     }
 
     public function bool(b: Bool): Void {
@@ -122,13 +87,10 @@ class Sip24 implements IHasher {
     }
 
     public function hash(): Int64 {
-        trace(buf.length);
         // padding for final word
         final wordCount = ((buf.length + 1) / 8).ceil();
         for (_ in buf.length...wordCount * 8 - 1) buf.addByte(0);
         buf.addByte(wordCount & 0xff);
-
-        final state = new Sip24State(k0, k1);
 
         // compression
         trace(buf.length);
@@ -136,9 +98,42 @@ class Sip24 implements IHasher {
         trace(data.length);
         for (i in 0...wordCount) {
             final w = data.getInt64(8 * i);
-            state.compress(w);
+            compress(w);
         }
 
-        return state.finalize();
+        return finalize();
+    }
+
+    inline function transform(): Void {
+        state.v0 += state.v1;
+        state.v1 = state.v1.rol(13);
+        state.v1 ^= state.v0;
+        state.v0 = state.v0.rol(32);
+        state.v2 += state.v3;
+        state.v3 = state.v3.rol(16);
+        state.v3 ^= state.v2;
+        state.v0 += state.v3;
+        state.v3 = state.v3.rol(21);
+        state.v3 ^= state.v0;
+        state.v2 += state.v1;
+        state.v1 = state.v1.rol(17);
+        state.v1 ^= state.v2;
+        state.v2 = state.v2.rol(32);
+    }
+
+    public inline function compress(word: Int64): Void {
+        state.v3 ^= word;
+        transform();
+        transform();
+        state.v0 ^= word;
+    }
+
+    public inline function finalize(): Int64 {
+        state.v2 ^= 0xff;
+        transform();
+        transform();
+        transform();
+        transform();
+        return state.v0 ^ state.v1 ^ state.v2 ^ state.v3;
     }
 }
